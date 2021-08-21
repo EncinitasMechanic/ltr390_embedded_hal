f0e5679205bdb5199c37f5f43c00347e561863fa
EncinitasMechanic <84678085+EncinitasMechanic@users.noreply.github.com>
2021-08-21 10:15:11 -0700

    mostly done

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,4 @@
-//TODO : don't forget PART_ID and MAIN_STATUS
-
 #![no_std]
-
-//use embedded_hal as hal;
-//use crate::hal::blocking::i2c::{Write, WriteRead};
-//use cast::{u8, u16};
 
 pub const CHIP_ADDRESS: u8 = 0x53;
 
@@ -52,7 +46,7 @@ pub enum Register {
     ALS_UVS_THRES_LOW_2 = 0x26,
 }
 
-impl Register{
+impl Register {
     #[inline(always)]
     pub fn bits(self) -> u8 {
         self as u8
@@ -111,7 +105,7 @@ pub enum Rate {
     TwoThousandMS = 0b0110,
 }
 
-impl Rate{
+impl Rate {
     #[inline(always)]
     pub fn bits(self) -> u8 {
         self as u8
@@ -166,54 +160,165 @@ impl InterruptPersist {
     }
 }
 
+#[derive(Debug)]
 pub struct Ltr390<I2C> {
     address: u8,
     bus: I2C,
 }
 
-impl<I2C, E> Ltr390<I2C> 
+impl<I2C, E> Ltr390<I2C>
 where
     I2C: embedded_hal::blocking::i2c::Write<Error = E>
         + embedded_hal::blocking::i2c::WriteRead<Error = E>,
 {
     #[inline(always)]
-    pub fn new(bus: I2C, address: u8) -> Self {
-        Ltr390 {
-            address,
-            bus,
-                }
+    pub fn new(bus: I2C, address: u8) -> Result<Self, E> {
+        let ltr390 = Self { address, bus };
+        Ok(ltr390)
     }
     #[inline(always)]
-    pub fn software_reset (&mut self) -> Result<(), E> {
-        self.bus.write(self.address, &[Register::MAIN_CTRL.bits(), MainCtrl::SoftwareReset.bits()])?;
+    pub fn software_reset(&mut self) -> Result<(), E> {
+        self.bus.write(
+            self.address,
+            &[Register::MAIN_CTRL.bits(), MainCtrl::SoftwareReset.bits()],
+        )?;
         Ok(())
     }
     #[inline(always)]
-    pub fn set_als_mode (&mut self) -> Result<(), E> {
-        self.bus.write(self.address, &[Register::MAIN_CTRL.bits(), MainCtrl::ALSMode.bits()])?;
+    pub fn set_als_mode(&mut self) -> Result<(), E> {
+        self.bus.write(
+            self.address,
+            &[Register::MAIN_CTRL.bits(), MainCtrl::ALSMode.bits()],
+        )?;
         Ok(())
     }
     #[inline(always)]
-    pub fn set_uvs_mode (&mut self) -> Result<(), E> {
-        self.bus.write(self.address, &[Register::MAIN_CTRL.bits(), MainCtrl::UVSMode.bits()])?;
+    pub fn set_uvs_mode(&mut self) -> Result<(), E> {
+        self.bus.write(
+            self.address,
+            &[Register::MAIN_CTRL.bits(), MainCtrl::UVSMode.bits()],
+        )?;
         Ok(())
     }
     #[inline(always)]
-    pub fn als_standby (&mut self) -> Result<(), E> {
-        self.bus.write(self.address, &[Register::MAIN_CTRL.bits(), MainCtrl::ALSStandby.bits()])?;
+    pub fn als_standby(&mut self) -> Result<(), E> {
+        self.bus.write(
+            self.address,
+            &[Register::MAIN_CTRL.bits(), MainCtrl::ALSStandby.bits()],
+        )?;
         Ok(())
     }
     #[inline(always)]
-    pub fn uvs_standby (&mut self) -> Result<(), E> {
-        self.bus.write(self.address, &[Register::MAIN_CTRL.bits(), MainCtrl::UVSStandby.bits()])?;
+    pub fn uvs_standby(&mut self) -> Result<(), E> {
+        self.bus.write(
+            self.address,
+            &[Register::MAIN_CTRL.bits(), MainCtrl::UVSStandby.bits()],
+        )?;
         Ok(())
     }
     #[inline(always)]
-    pub fn als_read (&mut self) -> Result<u32, E> {
+    pub fn main_status(&mut self) -> Result<u8, E> {
+        let mut buffer = [0 as u8, 1];
+        self.bus
+            .write_read(self.address, &[Register::MAIN_STATUS as u8], &mut buffer)?;
+        Ok(buffer[0] as u8)
+    }
+    #[inline(always)]
+    pub fn set_als_uvs_meas_rate(&mut self, resolution: Resolution, rate: Rate) -> Result<(), E> {
+        let data: u8 = resolution.bits() << 4 | rate.bits();
+        self.bus
+            .write(self.address, &[Register::ALS_UVS_MEAS_RATE.bits(), data])?;
+        Ok(())
+    }
+    #[inline(always)]
+    pub fn read_als_uvs_meas_rate(&mut self) -> Result<u8, E> {
+        let mut buffer = [0 as u8, 1];
+        self.bus.write_read(
+            self.address,
+            &[Register::ALS_UVS_MEAS_RATE.bits()],
+            &mut buffer,
+        )?;
+        Ok(buffer[0])
+    }
+    #[inline(always)]
+    pub fn set_als_uvs_gain(&mut self, gain: Gain) -> Result<(), E> {
+        let data: u8 = gain.bits();
+        self.bus
+            .write(self.address, &[Register::ALS_UVS_GAIN.bits(), data])?;
+        Ok(())
+    }
+    #[inline(always)]
+    pub fn read_als_uvs_gain(&mut self) -> Result<u8, E> {
+        let mut buffer = [0 as u8, 1];
+        self.bus
+            .write_read(self.address, &[Register::ALS_UVS_GAIN.bits()], &mut buffer)?;
+        Ok(buffer[0])
+    }
+    #[inline(always)]
+    pub fn part_id(&mut self) -> Result<[u8; 2], E> {
+        let mut buffer = [0 as u8, 1];
+        self.bus
+            .write_read(self.address, &[Register::PART_ID.bits()], &mut buffer)?;
+        let part_number = buffer[0] >> 4;
+        let revision_id = buffer[0] & 0x0f;
+        Ok([part_number, revision_id])
+    }
+    #[inline(always)]
+    pub fn als_read(&mut self) -> Result<u32, E> {
         let mut buffer = [0 as u8; 3];
-        self.bus.write_read(self.address, &[Register::ALS_DATA_0.bits(), Register::ALS_DATA_1.bits(), Register::ALS_DATA_2.bits() ], &mut buffer)?;
-        let als_reading: u32 = (buffer[2] as u32) << 16 | (buffer[1] as u32) << 8 | buffer[0] as u32;
-
+        self.bus
+            .write_read(self.address, &[Register::ALS_DATA_0.bits()], &mut buffer)?;
+        let als_reading: u32 =
+            (buffer[2] as u32) << 16 | (buffer[1] as u32) << 8 | buffer[0] as u32;
         Ok(als_reading)
+    }
+    #[inline(always)]
+    pub fn uvs_read(&mut self) -> Result<u32, E> {
+        let mut buffer = [0 as u8; 3];
+        self.bus
+            .write_read(self.address, &[Register::UVS_DATA_0.bits()], &mut buffer)?;
+        let uvs_reading: u32 =
+            (buffer[2] as u32) << 16 | (buffer[1] as u32) << 8 | buffer[0] as u32;
+        Ok(uvs_reading)
+    }
+    #[inline(always)]
+    pub fn interrupt_status(&mut self) -> Result<u8, E>{
+        let mut buffer = [0 as u8; 1];
+        self.bus.write_read(self.address, &[Register::INT_CFG.bits()], &mut buffer)?;
+        Ok(buffer[0])
+    }
+    #[inline(always)]
+    pub fn als_intterupt_enable(&mut self) -> Result<(), E>{
+        let data: u8 = InterruptConfig::ALSIntEnable.bits();
+        self.bus.write(self.address, &[Register::INT_CFG.bits(), data])?;
+        Ok(())
+    }
+    #[inline(always)]
+    pub fn als_intterupt_disable(&mut self) -> Result<(), E>{
+        let data: u8 = InterruptConfig::ALSIntDisable.bits();
+        self.bus.write(self.address, &[Register::INT_CFG.bits(), data])?;
+        Ok(())
+    }
+    #[inline(always)]
+    pub fn uvs_intterupt_enable(&mut self) -> Result<(), E>{
+        let data: u8 = InterruptConfig::UVSIntEnable.bits();
+        self.bus.write(self.address, &[Register::INT_CFG.bits(), data])?;
+        Ok(())
+    }
+    #[inline(always)]
+    pub fn uvs_intterupt_disable(&mut self) -> Result<(), E>{
+        let data: u8 = InterruptConfig::UVSIntDisable.bits();
+        self.bus.write(self.address, &[Register::INT_CFG.bits(), data])?;
+        Ok(())
+    }
+    #[inline(always)]
+    pub fn interrupt_persist(&mut self, rate: u8) -> Result<(), E>{
+        match rate{
+            0 => self.bus.write(self.address, &[Register::INT_PST.bits(), InterruptPersist::EveryThreshold.bits()])?,
+            2 => self.bus.write(self.address, &[Register::INT_PST.bits(), InterruptPersist::TwoConsecutive.bits()])?,
+            16 => self.bus.write(self.address, &[Register::INT_PST.bits(), InterruptPersist::SixteenConsecutive.bits()])?,
+            _ => self.bus.write(self.address, &[Register::INT_PST.bits(), InterruptPersist::EveryThreshold.bits()])?,
+            };
+        Ok(())
     }
 }
